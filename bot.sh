8c1f759eaa09696fd72a2d2ac747d1e5922b7a8a
dorianeselva <doriane.selva@gmail.com>
2014-01-16 14:33:27 +0100

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,30 +1,51 @@
+# Allow the user to configure his email address
+# Input : user
+# Output : file "config_email"
 chooseEmail()
 {
+    # Display the msg and read the answer of the user
     read -p "Veuillez saisir votre adresse email : " email
 
+    # Save the email written by the user
     echo $email >  config_email
 }
 
+
+# Allow the user to configure his dictionary
+# Input : user
+# Output : file "config_dico"
 chooseDictionary()
 {
     read -p "Veuillez saisir vos filtres séparés par des signe \"+\" : " dictionary
 
-    echo ${dictionary:0:-1} > config_dico
+    echo $dictionary > config_dico
 }
 
+
+# Allow the user to configure the crontab
 askCrontab()
 {
+   # Back up the crontab
+   crontab -l > mycron
+   
    read -p "Veuillez configurer la crontab : " cron
 
    echo "$cron cd /var/www/mynews/; sh /var/www/mynews/bot.sh -o=launch" > mycron
-
+    
+   # Install the new crontab
    crontab mycron
+   
+   echo "Votre configuration est sauvegardée"
 }
 
+
+# Fetch urls of articles according to the dictionary
 fetchUrls()
 {
+    # Store "config_dico" in the variable dictionary
     dictionary=$(<config_dico)
-
+    
+    # Stored the url of the research with the dictionary
     url="http://www.lemonde.fr/recherche/?keywords="
     url+=$dictionary
     url+="&page_num=1&operator=or&exclude_keywords=&qt=recherche_texte_titre&author=&period=for_1_week&start_day=01&start_month=01&start_year=1944&end_day=20&end_month=12&end_year=2013&sort=desc"
@@ -34,6 +55,7 @@ fetchUrls()
     grep "grid_3 alpha obf" site.html | awk -F "\"" '/grid_3 alpha obf/ {print"http://www.lemonde.fr"$2}' > urls;
 };
 
+# Fetch the contains of the articles
 fetchArticles()
 {
     echo "<html><head><title>Articles</title></head><body>" > articles;
@@ -47,6 +69,7 @@ fetchArticles()
     echo "</body></html>" >> articles;
 };
 
+# Send the articles
 sendArticles()
 {
     mail=$(<config_email)
@@ -69,22 +92,24 @@ do
     value=${param##*=}
 
     case $option in
-        -p | --password)        password=$value;;
-        -l | --login)           login=$value;;
-        -o | --operation)       operation=$value;;
+        -o | --operation)       operation=$value;
     esac
 done
 
+# Matching on the variable "operation"
+# The variable hasn't been defined
 if [ "$operation" == "" ]; then
     echo "Please choose an operation typing : sh $0 -o=my_operation"
 fi
 
+# The value is "config" : the programm launchs the 3 functions
 if [ "$operation" == "config" ]; then
     chooseEmail
     chooseDictionary
     askCrontab
 fi
 
+# The value is "launch" : the programm launchs the 3 functions
 if [ "$operation" == "launch" ]; then
     fetchUrls
     fetchArticles
